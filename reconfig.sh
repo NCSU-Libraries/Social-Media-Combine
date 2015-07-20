8805c86df22250b1ee47c9ac39ec52c3be3317ea
Jason Casden <jmcasden@ncsu.edu>
2015-07-20 09:11:11 -0400

    Shortern docker-compose timeout

--- reconfig.sh
+++ reconfig.sh
@@ -10,4 +10,4 @@ cp -n sfm/sfm_example_config.txt sfm/sfm_config.txt
 #docker-compose restart sfmapp
 
 echo "Recreating containers"
-docker-compose up -d --timeout 600
+docker-compose up -d --timeout 300
