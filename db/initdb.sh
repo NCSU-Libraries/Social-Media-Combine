29c7a4b93101e63bd8b7aebaa93629943cf971d3
Jason Casden <jmcasden@ncsu.edu>
2015-08-27 11:39:01 -0400

    Postgres container update

--- db/initdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "******CREATING DOCKER DATABASES******"
-gosu postgres postgres --single <<- EOSQL
-CREATE DATABASE sfm;
-CREATE DATABASE lentil;
-EOSQL
-echo ""
-echo "******DOCKER DATABASES CREATED******"
