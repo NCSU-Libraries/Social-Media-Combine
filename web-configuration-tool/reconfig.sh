34e3d1a480c6a847a0624bbadbc610156c97fba3
Mitul Panchal <mpancha@ncsu.edu>
2015-09-11 14:46:37 -0400

    Enabled Docker Provider and fixed MYDB environment variable

--- web-configuration-tool/reconfig.sh
+++ web-configuration-tool/reconfig.sh
@@ -2,3 +2,11 @@
 cd /src/config_updates
 
 touch configs.updated
+# if env var says docker provider
+# restart container from here
+docker stop lentilapp
+docker stop sfmapp
+
+docker start lentilapp
+docker start sfmapp
+echo "Containers reconfigured!"
